3261a3a09787d5ee167762d5657879240bcb5854
Jorge Antunes <jmpdantunes@gmail.com>
2020-01-21 14:26:58 +0000

    Modelo: Question Service: Question

--- src/app/questionnaire/models/question/question.ts
+++ src/app/questionnaire/models/question/question.ts
@@ -1,2 +1,12 @@
 export class Question {
+
+    'question': string;
+    'aType': string;
+    'rightAnswer': number;
+    'options': string[];
+    'role': string;
+    
+    constructor(data?: any) {
+        Object.assign(this, data);
+    }
 }
