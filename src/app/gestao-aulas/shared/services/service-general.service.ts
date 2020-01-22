17c52741e52af369ec2a6bb3b0c5d31cdfb51d53
tifipe <tiago_preto_20@hotmail.com>
2020-01-22 18:50:29 +0000

    model edition add lesson

--- src/app/gestao-aulas/shared/services/service-general.service.ts
+++ src/app/gestao-aulas/shared/services/service-general.service.ts
@@ -3,6 +3,7 @@ import { Account } from '../models/account';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
+import { Edition } from '../models/edition';
 
 @Injectable({
   providedIn: 'root'
@@ -13,24 +14,35 @@ export class ServiceGeneralService {
 
   private currentAccount: Account = new Account();
   private userApi: UserServiceService;
+ /*  private edtion: Edition[] = new Edition; */
 
   constructor(
     private router: Router,
     private http: HttpClient,
   ) { 
     this.currentAccount.userID = this.userApi.getCurrentUser().id
-    this.getEditiond().subscribe(
+    this.getAccount().subscribe(
       (account: Account) => {
         this.currentAccount=account;
         }
       )
   }
 
-  public getEditiond(){
+  public getAccount(){
     return this.http.get(this.url + this.currentAccount.userID)
   }
 
+/*   public getEditions(){
+    this.
+  } */
   
+
+
+
+
+
+
+
 
 
  
