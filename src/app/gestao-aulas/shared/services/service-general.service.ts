43aef2187ed8f3715c98d5ca20ef42908fabd639
tifipe <tiago_preto_20@hotmail.com>
2020-01-22 16:48:48 +0000

    add account service get userid and get account

--- src/app/gestao-aulas/shared/services/service-general.service.ts
+++ src/app/gestao-aulas/shared/services/service-general.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Account } from '../models/account';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceGeneralService {
 
-  constructor() { }
+  private url ='http://localhost:8080/coreFinalProject/aulas/account/'
+
+  private currentAccount: Account = new Account();
+  private userApi: UserServiceService;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) { 
+    this.currentAccount.userID = this.userApi.getCurrentUser().id
+    this.getEditiond().subscribe(
+      (account: Account) => {
+        this.currentAccount=account;
+        }
+      )
+  }
+
+  public getEditiond(){
+    return this.http.get(this.url + this.currentAccount.userID)
+  }
+
+  
+
+
+ 
 }
