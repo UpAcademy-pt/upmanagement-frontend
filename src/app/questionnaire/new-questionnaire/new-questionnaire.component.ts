a4a58dc546206c6483689688f89e8b77fe080d86
Jorge Antunes <jmpdantunes@gmail.com>
2020-01-23 02:29:11 +0000

    Adicionar formulário feio, mas a funcionar

--- src/app/questionnaire/new-questionnaire/new-questionnaire.component.ts
+++ src/app/questionnaire/new-questionnaire/new-questionnaire.component.ts
@@ -19,16 +19,39 @@ export class NewQuestionnaireComponent implements OnInit {
  private a: Question = new Question();
  private b: Questionnaire = new Questionnaire();
 
+ //private questions: Question[] = this.b.questionList;
+
   public addQuestion(question: Question) {
     console.log('data');
     console.log(question);
+    console.log(this.b);
     this.questionService.createQuestion(question).subscribe();
+    if (this.b.questionList != undefined) {
+      this.b.questionList.push(question);
+    } else {
+      this.b.questionList = [question];
+    }
+    console.log(this.b.questionList);
+    this.a = new Question(); //ver se existe uma forma melhor
+  }
+
+  public addMoreOptions(option: string){
+    console.log(option)
+    if (this.a.options != undefined) {
+      this.a.options.push(option);
+    } else {
+      this.a.options = [option];
+    }
+    option = "";
   }
 
   public addQuestionnaire(questionnaire: Questionnaire) {
     console.log('data');
     console.log(questionnaire);
+
     //this.questionnaireService.createQuestionnaire(questionnaire).subscribe();
+
+    this.b = new Questionnaire(); //ver se existe uma forma melhor
   }
 
   public addNewQuestionOnForm(){
