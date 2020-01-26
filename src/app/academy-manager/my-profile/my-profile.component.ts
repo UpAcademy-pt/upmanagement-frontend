ac35810ab5e71c6ffbbf844a92be9f24d95402b8
CristianoVale <cristiano.fcvale@gmail.com>
2020-01-26 17:02:44 +0000

    init User Profile

--- src/app/academy-manager/my-profile/my-profile.component.ts
+++ src/app/academy-manager/my-profile/my-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-my-profile',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  public name: string;
+
+  profileForm  = new FormGroup({
+    editable: new FormControl(),
+    notEditable: new FormControl(),
+  });
+
+  constructor(
+    private userApi: UserServiceService
+  ) 
+  {
+    this.name = this.userApi.getCurrentName();
+    this.profileForm.controls['notEditable'].disable();
+    this.profileForm.controls['editable'].disable();
+    
+   }
 
   ngOnInit() {
   }
 
+  public MakeEdit(){
+    this.profileForm.controls['editable'].enable();
+    this.toggleShow();
+  }
+
+isShown: boolean = false ; // hidden by default
+toggleShow() {
+
+this.isShown = ! this.isShown;
+
 }
+
+}
+
