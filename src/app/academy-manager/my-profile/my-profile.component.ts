f864f98ebf3dd5e34fa6a02951dd56e21c145606
CristianoVale <cristiano.fcvale@gmail.com>
2020-01-26 18:54:16 +0000

    Implemented Faltas

--- src/app/academy-manager/my-profile/my-profile.component.ts
+++ src/app/academy-manager/my-profile/my-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-my-profile',
@@ -10,19 +11,25 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class MyProfileComponent implements OnInit {
 
   public name: string;
+  public email: string;
+  readonly URL ="http://localhost:8080/coreFinalProject/academy-manager/missed/account/1";
+  misses: any;
 
   profileForm  = new FormGroup({
     editable: new FormControl(),
-    notEditable: new FormControl(),
+    notEditable: new FormControl()
   });
 
   constructor(
-    private userApi: UserServiceService
-  ) 
+    private userApi: UserServiceService,
+    private http: HttpClient
+  )
   {
     this.name = this.userApi.getCurrentName();
+    this.email = this.userApi.getCurrentEmail();
     this.profileForm.controls['notEditable'].disable();
     this.profileForm.controls['editable'].disable();
+    this.getMisses();
     
    }
 
@@ -32,6 +39,10 @@ export class MyProfileComponent implements OnInit {
   public MakeEdit(){
     this.profileForm.controls['editable'].enable();
     this.toggleShow();
+  }
+
+  getMisses(){
+    this.misses = this.http.get(this.URL);
   }
 
 isShown: boolean = false ; // hidden by default
