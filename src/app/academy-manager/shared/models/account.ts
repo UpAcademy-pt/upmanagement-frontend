6a5243cf6af59356940c2f396674a5ceb11f1d9f
rute-m-t <rute.m.t@gmail.com>
2020-01-27 16:37:16 +0000

    created model theme, added it to model account

--- src/app/academy-manager/shared/models/account.ts
+++ src/app/academy-manager/shared/models/account.ts
@@ -1,15 +1,16 @@
+import { Theme } from './theme';
+
 export class Account {
     'id': number;
     'userId': number;
     'age': number;
-    'academyIds': number[];
+    'academyIds': number[] = [];
     'academicDegree': string;
     'academicBackground': string;
     'photoLink': string;
     'mobilePhone': number;
     'linkedInAdress': string;
-    'themeIds': number[];
-    'missedDays': string;
+    'themes': Theme[] = [];
     'nif': number;
 
     constructor(data?: any) {
---
+++ src/app/academy-manager/shared/models/theme.ts
@@ -0,0 +1,8 @@
+export class Theme {
+    'id': number;
+    'name': String;
+
+    constructor(data?: any) {
+        Object.assign(this, data);
+      }
+}
