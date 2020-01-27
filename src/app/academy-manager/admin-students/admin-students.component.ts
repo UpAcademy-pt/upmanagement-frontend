4167bb8555412a9fe92e8d0a5461164dc487e208
rute-m-t <rute.m.t@gmail.com>
2020-01-27 01:42:40 +0000

    added partial profile to admin students and teachers; changed tables' structure

--- src/app/academy-manager/admin-students/admin-students.component.ts
+++ src/app/academy-manager/admin-students/admin-students.component.ts
@@ -3,7 +3,8 @@ import { ReplaySubject } from 'rxjs';
 import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
 import { User } from 'src/app/core/models/user';
 import { AccountService } from '../shared/services/account.service';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { BsModalRef, BsModalService, TabHeadingDirective } from 'ngx-bootstrap';
+import { Account } from '../shared/models/account';
 
 @Component({
   selector: 'app-admin-students',
@@ -15,9 +16,16 @@ export class AdminStudentsComponent implements OnInit {
   modalRef: BsModalRef;
   private studentUsers: User[];
   public studentUsers$: ReplaySubject<User[]> = new ReplaySubject(1);
-  private studentAccounts: Account[] = [];
-  public studentAccounts$: ReplaySubject<Account[]> = new ReplaySubject(1);
-  public accountToShow: Account;
+  /* private studentAccounts: Account[] = [];
+  public studentAccounts$: ReplaySubject<Account[]> = new ReplaySubject(1); */
+  private studentUserAccounts: {}[] = [];
+  public studentUserAccounts$: ReplaySubject<{}[]> = new ReplaySubject(1);
+  public accountToUpdate: Account;
+  public userToShow: User;
+  public inUpdate = false;
+  public index: number;
+  public confirmDelete = false;
+
 
   constructor(
     private accountService: AccountService,
@@ -25,32 +33,65 @@ export class AdminStudentsComponent implements OnInit {
     private modalService: BsModalService
   ) {
     this.getAllStudents();
-    
   }
 
   ngOnInit() {
   }
 
   public getAllStudents() {
-    this.userService.getUsers('','','USER').subscribe(
-      (res:any) => {
-      this.studentUsers = res;
-      this.studentUsers$.next(res);
-      this.studentUsers.forEach(student => this.getStudentAccount(student));
+    this.userService.getUsers('', '', 'USER').subscribe(
+      (res: any) => {
+        this.studentUsers = res;
+        this.studentUsers$.next(res);
+        this.studentUsers.forEach(student => this.getStudentAccount(student));
       }
     )
   }
 
   public getStudentAccount(studentUser: User) {
-    this.accountService.getByUserId(studentUser.id).subscribe((res:any) => {
-      this.studentAccounts.push(res);
-      this.studentAccounts$.next(this.studentAccounts);
-      });
+    this.accountService.getByUserId(studentUser.id).subscribe((account: any) => {
+      if (account !== null) {
+        this.studentUserAccounts.push({ 'studentUser': studentUser, 'studentAccount': account });
+        this.studentUserAccounts$.next(this.studentUserAccounts);
+      }
+    });
   }
 
-  openModalShowAccount(template: TemplateRef<any>, rowIndex: number) {
-    this.accountToShow = {...this.studentAccounts[rowIndex]};
-    this.modalRef = this.modalService.show(template);
+  public openModalShowAccount(template: TemplateRef<any>, rowIndex: number) {
+    this.accountToUpdate = { ...this.studentUserAccounts[rowIndex]['studentAccount'] };
+    this.userToShow = { ...this.studentUserAccounts[rowIndex]['studentUser'] };
+    this.modalRef = this.modalService.show(template, Object.assign({}, { class: 'modal-lg' }));
+    this.index = rowIndex;
+  }
+
+  public toggleUpdateAccount() {
+    this.inUpdate = true;
+  }
+
+  public updateStudentAccount() {
+    this.accountService.update(this.accountToUpdate).subscribe(
+      (msg: string) => {
+        this.studentUserAccounts[this.index]['studentAccount'] = this.accountToUpdate;
+        this.inUpdate = false;
+      }
+    );
+  }
+
+  public askToConfirmDelete() {
+    this.confirmDelete = !this.confirmDelete;
+  }
+
+  public deleteStudentAccount() {
+    this.accountService.delete(this.accountToUpdate.id).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.accountToUpdate = new Account();
+        this.userToShow = new User();
+        this.studentUserAccounts.splice(this.index, 1);
+        this.studentUserAccounts$.next(this.studentUserAccounts);
+        this.modalRef.hide();
+        this.confirmDelete = !this.confirmDelete;
+      });
   }
 
 }
--- src/app/academy-manager/admin-teachers/admin-teachers.component.ts
+++ src/app/academy-manager/admin-teachers/admin-teachers.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
 import { ReplaySubject } from 'rxjs';
 import { User } from 'src/app/core/models/user';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { AccountService } from '../shared/services/account.service';
+import { Account } from '../shared/models/account';
 
 @Component({
   selector: 'app-admin-teachers',
@@ -10,10 +13,21 @@ import { User } from 'src/app/core/models/user';
 })
 export class AdminTeachersComponent implements OnInit {
 
-  public teachers$: ReplaySubject<User[]> = new ReplaySubject(1);
+  modalRef: BsModalRef;
+  private teacherUsers: User[];
+  public teacherUsers$: ReplaySubject<User[]> = new ReplaySubject(1);
+  private teacherUserAccounts: {}[] = [];
+  public teacherUserAccounts$: ReplaySubject<{}[]> = new ReplaySubject(1);
+  public accountToUpdate: Account;
+  public userToShow: User;
+  public inUpdate = false;
+  public index: number;
+  public confirmDelete = false;
 
   constructor(
-    private userService: UserServiceService
+    private userService: UserServiceService,
+    private accountService: AccountService,
+    private modalService: BsModalService
   ) {
     this.getAllTeachers();
   }
@@ -24,9 +38,57 @@ export class AdminTeachersComponent implements OnInit {
   public getAllTeachers() {
     this.userService.getUsers('','','SUPERUSER').subscribe(
       (res:any) => {
-      this.teachers$.next(res)
+        this.teacherUsers = res;
+        this.teacherUsers$.next(res);
+        this.teacherUsers.forEach(teacher => this.getTeacherAccount(teacher));
       }
-    )
+    );
+  }
+
+  public getTeacherAccount(teacherUser: User) {
+    this.accountService.getByUserId(teacherUser.id).subscribe((account: any) => {
+      if (account !== null) {
+        this.teacherUserAccounts.push({ 'teacherUser': teacherUser, 'teacherAccount': account });
+        this.teacherUserAccounts$.next(this.teacherUserAccounts);
+      }
+    });
+  }
+
+  public openModalShowAccount(template: TemplateRef<any>, rowIndex: number) {
+    this.accountToUpdate = { ...this.teacherUserAccounts[rowIndex]['teacherAccount'] };
+    this.userToShow = { ...this.teacherUserAccounts[rowIndex]['teacherUser'] };
+    this.modalRef = this.modalService.show(template, Object.assign({}, { class: 'modal-lg' }));
+    this.index = rowIndex;
+  }
+
+  public toggleUpdateAccount() {
+    this.inUpdate = true;
+  }
+
+  public updateTeacherAccount() {
+    this.accountService.update(this.accountToUpdate).subscribe(
+      (msg: string) => {
+        this.teacherUserAccounts[this.index]['teacherAccount'] = this.accountToUpdate;
+        this.inUpdate = false;
+      }
+    );
+  }
+
+  public askToConfirmDelete() {
+    this.confirmDelete = !this.confirmDelete;
+  }
+
+  public deleteTeacherAccount() {
+    this.accountService.delete(this.accountToUpdate.id).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.accountToUpdate = new Account();
+        this.userToShow = new User();
+        this.teacherUserAccounts.splice(this.index, 1);
+        this.teacherUserAccounts$.next(this.teacherUserAccounts);
+        this.modalRef.hide();
+        this.confirmDelete = !this.confirmDelete;
+      });
   }
 
 }
