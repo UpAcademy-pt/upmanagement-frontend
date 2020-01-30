185d9b971a22062812c0ba62e6dbfc1fe7ff4f26
martinalferreira <martina.lopes19@gmail.com>
2020-01-30 23:38:27 +0000

    started implementing evaluations table

--- src/app/academy-manager/modules/modules.component.ts
+++ src/app/academy-manager/modules/modules.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
 import { ReplaySubject } from 'rxjs';
 import { User } from 'src/app/core/models/user';
+import { ModuleService } from '../shared/services/module.service';
+import { EvaluationService } from '../shared/services/evaluation.service';
+import { GradeService } from '../shared/services/grade.service';
+import { Module } from '../shared/models/module';
+import { Evaluation } from '../shared/models/evaluation';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Grade } from '../shared/models/grade';
 
 @Component({
   selector: 'app-modules',
@@ -11,11 +18,35 @@ import { User } from 'src/app/core/models/user';
 })
 export class ModulesComponent implements OnInit {
 
-  public students$: ReplaySubject<User[]> = new ReplaySubject(1);
+  private students$: ReplaySubject<User[]> = new ReplaySubject(1);
+  private module: Module;
+  private module$: ReplaySubject<Module> = new ReplaySubject(1);
+  private evaluations: Evaluation[] = [];
+  private evaluations$: ReplaySubject<Evaluation[]> = new ReplaySubject(1);
+
+  public evaluationToCreate: Evaluation = new Evaluation();
+  public grade: Grade;
+  public gradesArray: Grade[];
+
+  modalRef: BsModalRef;
 
   constructor(
-    private userService: UserServiceService
+    private route: ActivatedRoute,
+    private userService: UserServiceService,
+    private moduleService: ModuleService,
+    private evaluationService: EvaluationService,
+    private gradeService: GradeService,
+    private modalService: BsModalService
   ) {
+    this.route.params.subscribe(
+      params => {
+        this.moduleService.getbyId(Number(params.moduleId)).subscribe(
+          (module: Module) => {
+            this.module = module;
+            this.module$.next(this.module);
+            this.getEvaluationsByModuleId(this.module.evaluationIds);
+          }
+        ); });
     this.getAllStudents();
   }
 
@@ -30,4 +61,39 @@ export class ModulesComponent implements OnInit {
     );
   }
 
+  public getEvaluationsByModuleId(idArray: number[]) {
+    idArray.forEach(id => {
+      this.evaluationService.getbyId(id).subscribe(
+        (evaluation: Evaluation) => {
+          this.evaluations.push(evaluation);
+          this.evaluations$.next(this.evaluations);
+        }
+      );
+    });
+  }
+
+  public createEvaluation(grades: Grade[]) {
+    this.evaluationToCreate.grades = this.gradesArray;
+    this.evaluationService.createEvaluation(this.evaluationToCreate).subscribe(
+      (msg: string) => {
+       console.log(this.evaluationToCreate);
+      }
+    );
+    this.modalRef.hide();
+    this.evaluationToCreate = new Evaluation();
+  }
+
+  public createGrades(subject: string) {
+    this.grade.subject = subject;
+    this.gradeService.createGrade(this.grade).subscribe(
+      (id: number) => {
+        this.grade.id = id;
+        this.gradesArray.push(this.grade);
+      }
+    );
+  }
+
+  openModalAddEvaluation(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
